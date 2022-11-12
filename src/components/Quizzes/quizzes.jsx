44bfa62a8092b9997301337318f183bd6e20210f
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2022-11-12 14:13:06 -0500

    first commit

---
+++ src/components/Quizzes/quizzes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Quizzes = () => {
+  return (
+    <div>
+        <div className= 'quiz-container' >
+
+
+
+     
+
+        </div>
+
+
+    </div>
+  )
+}
