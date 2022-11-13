9593d58e6fb0f3676e121faaef0d4784b3a03ec6
BettyHoPro <bettyvisualtech@gmail.com>
2022-11-12 16:45:38 -0800

    feat: the state and basic function

--- src/components/Dictionary/dictionary.jsx
+++ src/components/Dictionary/dictionary.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Axios from 'axios';
+import { MdSearch } from 'react-icons/md';
+import './dictionary.css'
 
 export const Dictionary = () => {
+  const [data, setData] = useState("");
+  const [ searchWord, setSearchWord ] = useState("");
+
+  function translate () {
+
+    Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`).then( (response) => {
+      setData(response.data[0]);
+    })
+  }
   return (
-    <div> 
+    <div className="mainDic"> 
       
       <h1> Integrated Word Finder</h1>
-      
-      
+      <div className="searchBar">
+        <input type="text" placeholder="Search the word..." onChange = { e => setSearchWord(e.target.value)} />
+        <button onClick = {() => translate()}>
+          <MdSearch size="10 px" />
+        </button>
+
+      </div>
+      <p class="main">Test!</p>
+      <Button>Search</Button>
     
      </div>
   )
