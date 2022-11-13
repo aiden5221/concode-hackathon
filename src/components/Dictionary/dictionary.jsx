af2aa6a0fbc4245a27d7fa480be0b6232f0cb465
BettyHoPro <bettyvisualtech@gmail.com>
2022-11-12 19:10:43 -0800

    prograss: MUI - search icon btn

--- src/components/Dictionary/dictionary.jsx
+++ src/components/Dictionary/dictionary.jsx
@@ -3,7 +3,14 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 import axios from 'axios';
 import { MdSearch } from 'react-icons/md';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
+import { styled, alpha } from '@mui/material/styles';
 import './dictionary.css'
+
+import { OutlinedInput } from '@mui/material';
 
 export const Dictionary = () => {
   const [result, setData] = useState("");
@@ -11,49 +18,94 @@ export const Dictionary = () => {
 
   function translate () {
 
-    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`).then( (response) => {
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`).then( (response) => {
       setData(response.data[0]);
     })
   }
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    color: 'inherit',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+ 
+  
+
   return (
+    
     <div className="mainDic"> 
       
       <h1> Integrated Word Finder</h1>
+    
       <div className="searchBar">
-        <input type="text" placeholder="Search the word..." onChange = { e => setSearchWord(e.target.value)} />
+      <div
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Search>
+      <TextField id="outlined-basic" label="Search" variant="outlined" placeholder="Search the word..." onChange = { e => setSearchWord(e.target.value)}/>
+      {/* <button onClick = {() => translate()}>
+          <MdSearch size="10 px" />
+        </button> */}
+      {/* <button id="searchBtn" onClick = {() => translate()}> */}
+      <SearchIconWrapper id="searchBtn" ><SearchIcon onClick = {() => translate()}/></SearchIconWrapper>
+        {/* </button> */}
+        </Search>
+      
+    </div>
+      {/* <input id= "filled-basic" type="text" placeholder="Search the word..." onChange = { e => setSearchWord(e.target.value)} />
         <button onClick = {() => translate()}>
           <MdSearch size="10 px" />
-        </button>
+        </button> */}
       </div>
-
-      {result && (
+     
+       {result && (
        <div className="resultList">
         <h2>
           {result.word}{" "}
         </h2>
-        {/* <p4>Phonetic:</p4> */}
+  
         <p>{result.phonetic}</p>
 
         <h4>Definitions:</h4>
         <p>{result.meanings[0].definitions[0].definition}</p>
-        <p>{result.meanings[0].definitions[1].definition}</p>
-        <p>{result.meanings[0].definitions[2].definition}</p>
+        {/* <p>{result.meanings[0].definitions[1].definition}</p> */}
+        {/* <p>{result.meanings[0].definitions[2].definition}</p> */}
         <h4>Example:</h4>
         <p>{result.meanings[0].definitions[0].examples}</p>
-        <p>{result.meanings[0].definitions[1].examples}</p>
-        <p>{result.meanings[0].definitions[2].examples}</p>
+        {/* <p>{result.meanings[0].definitions[1].examples}</p>
+        <p>{result.meanings[0].definitions[2].examples}</p> */}
 
        </div>
-
+     
       )} 
-      {/* { !result && (
-        <div className="resultList">
-        <h4>
-          There is no such word in this dictionary
-        </h4>
-       </div>
- 
-      )} */}
+  
      </div>
   )
 }
