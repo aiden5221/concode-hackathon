7c2d9587855507685c104f1d12f5ed954b291f0b
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2022-11-13 02:11:16 -0500

    merge

--- src/components/Thesaurus/thesaurus.jsx
+++ src/components/Thesaurus/thesaurus.jsx
@@ -12,11 +12,11 @@ export const Thesaurus = () => {
   return (
     <Box>
     <Typography variant='h4' sx={{textAlign:'center'}}> Improve your Vocabulary</Typography>
-   <Box pl={{xs:'none', md:4}} pb={{xs:'none',md:'10',lg:'10'}} pt={{xs:'none',md:'3'}} pr={3} sx={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-    <Grid container ><Cards/></Grid>
-    <Grid container ><Card2s/></Grid>
-    <Grid container ><Card3s/></Grid>
-    <Grid container ><Card4s/></Grid>
+   <Box pl={{xs:0, md:0, lg:10,xl:4}} gap={3} pb={10} pt={3} pr={10} sx={{display:{lg:'flex', sm: 'none' ,xl:'flex',md:'none'},justifyContent:'center', alignItems:'center',flexDirection:{sm:'none',lg:'none',md:'none',xl:'none'}}}>
+    <Grid><Cards/></Grid>
+    <Grid><Card2s/></Grid>
+    <Grid><Card3s/></Grid>
+    <Grid><Card4s/></Grid>
    </Box> 
    </Box>
 
